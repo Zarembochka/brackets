8680694162e4a56c9d7839cd338e6b6ea23c8f35
Luba Hadunova <luba.godunova2018@gmail.com>
2023-07-26 11:47:29 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  const brackets = Object.fromEntries(bracketsConfig);
+  const bracketsKeys = Object.keys(brackets);
+  const bracketsValues = Object.values(brackets);
+
+  const arrayForHelp = [];
+  var theSameBrackets = '';
+
+  for (let char of str) {
+        if (Object.keys(brackets).includes(char) && !Object.values(brackets).includes(char)) {
+            arrayForHelp.push(char);
+            continue;
+        }
+
+        if (Object.keys(brackets).includes(char) && Object.values(brackets).includes(char)) {
+            if (char === theSameBrackets) {
+                if (char === arrayForHelp.at(-1)) {
+                    arrayForHelp.pop();
+                    theSameBrackets = arrayForHelp.at(-1) ?? '';
+                    continue;
+                }
+            }
+
+            arrayForHelp.push(char);
+            theSameBrackets = char;
+            continue;
+        }
+
+        const bracketFirst = bracketsKeys[bracketsValues.indexOf(char)];
+        if (bracketFirst === arrayForHelp.at(-1)) {
+            arrayForHelp.pop();
+            continue;
+        }
+
+        return false;
+    }
+
+    return (arrayForHelp.length === 0);
 }
